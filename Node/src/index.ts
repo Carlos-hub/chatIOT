aa8ce05a9c63cdf1d6e88cbed99887a4e03fddaa
Carlos-hub <delaracarlosmiguel@gmail.com>
2023-04-11 20:33:55 -0300

    ajustando cors

--- Node/src/index.ts
+++ Node/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import { createServer } from 'http';
 import { Server as SocketIOServer } from 'socket.io';
-
+import cors from 'cors';
 require('dotenv').config();
 dotenv.config();
 const port:string | undefined | any = process.env.PORT_SOCKET
@@ -19,14 +19,16 @@ const io: SocketIOServer = require('socket.io')(httpServer, {
         username:string;
         room: string[]
     }
-app.use(express.json())
+app.use(express.json());
+app.use(cors({
+    origin:['http://localhost:5173','https://chat-front-wjyy.vercel.app/']
+   }))
 
 io.listen( port | 4999);
 let users:IUser[] =[];
 var room:object[]= [];
 
 io.on('connection', (socket:any) =>{
-    const connection = socket.id;
     console.log(socket.id);
 
     socket.on('joinRoom',(data:string) =>{
