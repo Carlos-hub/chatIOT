2e7c6153f7503be13ebb7ba9aaa62dfcb28416bd
Carlos-hub <delaracarlosmiguel@gmail.com>
2023-04-11 20:04:07 -0300

    ajustando eventos do socket

--- Node/src/index.ts
+++ Node/src/index.ts
@@ -2,8 +2,6 @@ import express from "express";
 import dotenv from 'dotenv';
 import { createServer } from 'http';
 import { Server as SocketIOServer } from 'socket.io';
-import { log } from "console";
-import { any } from "webidl-conversions";
 dotenv.config();
 const app = express();
 const httpServer = createServer(app);
@@ -28,25 +26,18 @@ io.on('connection', (socket:any) =>{
     const connection = socket.id;
     console.log(socket.id);
 
-    socket.on('join_room',(data:string) =>{
+    socket.on('joinRoom',(data:string) =>{
         socket.join(data);
         console.log('====================================');
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
-        console.log('====================================');
     });
     
     
     socket.on('sendMessage',(data:any)=>{
         console.log(data);
-        socket.to(data.room).emit('receive_message',data);
-        room.push(data);
+        socket.to(data.room).emit('receiveMessage',data);
     });
 
-// socket.on('userConnect',(user:any)=>{
-//     console.log(user);
-//     users.push({id:socket.id,username:user,room:[user]});
-//     socket.join(user);
-// });
 socket.emit('getUsers',users);
 
 
